2caab366395e945bcbb46163b38b080459fed2ef
mitsuru1397 <g1527068@st.t-kougei.ac.jp>
2016-06-27 17:20:10 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,7 +1,7 @@
 for(var i = 1; i < 101; i++){
 var li = document.createElement('li');
 
-if( i % 3 == 0 && i % 5 ==5){
+if( i % 3 == 0 && i % 5 ==0){
 li.textContent = 'FizzBuzz';
 }
 else if( i % 3 == 0){
