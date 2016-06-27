d6f4cdae624ad10de5c57dcda017d5e01b96b04b
mitsuru1397 <g1527068@st.t-kougei.ac.jp>
2016-06-27 17:16:24 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,9 @@
 for(var i = 1; i < 101; i++){
 var li = document.createElement('li');
+
+if( i % 3 == 0 && i % 5 ==5){
 li.textContent = i + '回目';
+}
+
 document.getElementById('list').appendChild(li);
 }
