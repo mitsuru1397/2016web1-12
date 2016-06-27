549c0bf9c4c025995e68d62424ae4c6f8e332d2b
mitsuru1397 <g1527068@st.t-kougei.ac.jp>
2016-06-27 17:17:08 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,8 +2,9 @@ for(var i = 1; i < 101; i++){
 var li = document.createElement('li');
 
 if( i % 3 == 0 && i % 5 ==5){
-li.textContent = i + '回目';
+li.textContent = 'FizzBuzz';
 }
-
+else
+li.textContent = i + '回目';
 document.getElementById('list').appendChild(li);
 }
